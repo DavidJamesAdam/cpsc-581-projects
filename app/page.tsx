4414e29a03874d483718b4ed75377ea643ea94d4
DavidJamesAdam <davidjamesadam@gmail.com>
2026-01-28 14:49:16 -0700

    feature: changed bedroom image to something that might work better

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Image
-        src="/446.jpg"
+        src="/cartoon_bedroom.webp"
         alt="Background image"
         quality={100}
         fill
