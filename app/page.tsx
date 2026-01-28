bd8521ee050f48e8f28fded3edf09c6889378104
DavidJamesAdam <davidjamesadam@gmail.com>
2026-01-28 14:45:59 -0700

    feature: added bg image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import DavidButton from "./components/buttons/david-button/david-button";
 import HankButton from "./components/buttons/hank-button/hank-button";
 import CollinButton from "./components/buttons/collin-button/collin-button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div>
+      <Image
+        src="/446.jpg"
+        alt="Background image"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+          zIndex: -1,
+        }}
+      />
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <DavidButton />
         <HankButton />
         <CollinButton />
